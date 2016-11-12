a512e431c6fd6ef1a1dda52a02f9c972b2c8f86b
weapon <weaponhe@126.com>
2016-11-12 17:50:42 +0800

    FIX:production error

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  callbackURL: "http://121.42.214.145:3000/auth/github/callback"
   // callbackURL: "/auth/github/callback"
 }, function (accessToken, refreshToken, profile, done) {
   token = accessToken;
